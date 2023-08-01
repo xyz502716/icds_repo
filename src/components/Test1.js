e246c97bd37d34de6c690f47e5bc76442a693f99
xyz502716 <xyz502716@gmail.com>
2023-08-01 17:29:21 +0100

    Different data added

--- src/components/Test1.js
+++ src/components/Test1.js
@@ -1,27 +1,77 @@
 import React from 'react';
 //import ReactDOM from 'react-dom';
-import { VictoryBar, VictoryChart, VictoryAxis } from "victory";
+import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme, VictoryStack } from 'victory';
 
 const Test1 = () => {
 
-    const data = [
-        { quarter: 1, earnings: 10000 },
-        { quarter: 2, earnings: 13500 },
-        { quarter: 3, earnings: 15250 },
+    const data2012 = [
+        { quarter: 1, earnings: 13000 },
+        { quarter: 2, earnings: 16500 },
+        { quarter: 3, earnings: 14250 },
         { quarter: 4, earnings: 19000 }
+    ];
+
+    const data2013 = [
+        { quarter: 1, earnings: 15000 },
+        { quarter: 2, earnings: 12500 },
+        { quarter: 3, earnings: 19500 },
+        { quarter: 4, earnings: 13000 }
+    ];
+
+    const data2014 = [
+        { quarter: 1, earnings: 11500 },
+        { quarter: 2, earnings: 13250 },
+        { quarter: 3, earnings: 20000 },
+        { quarter: 4, earnings: 15500 }
+    ];
+
+    const data2015 = [
+        { quarter: 1, earnings: 18000 },
+        { quarter: 2, earnings: 13250 },
+        { quarter: 3, earnings: 15000 },
+        { quarter: 4, earnings: 12000 }
     ];
 
     return (
         <div>
             <h1>Victory Tutorial</h1>
-            <VictoryChart domainPadding={20}>
-                <VictoryAxis tickValues={[1, 2, 3, 4]}
-                    tickFormat={["Quarter 1", "Quarter 2", "Quarter 3", "Quarter 4"]}
+            <VictoryChart
+                domainPadding={10}
+                theme={VictoryTheme.material}
+            >
+                <VictoryAxis
+                    tickValues={["Quarter 1", "Quarter 2", "Quarter 3", "Quarter 4"]}
                 />
-                <VictoryBar data={data} x="quarter" y="earnings" />
-            <VictoryAxis dependentAxis tickFormat={(x) => (`$${x / 1000}k`)} />
-        </VictoryChart>
-        </div >
+                <VictoryAxis
+                    dependentAxis
+                    tickFormat={(x) => (`$${x / 1000}k`)}
+                />
+                <VictoryStack
+                    colorScale={"warm"}
+                >
+                    <VictoryBar
+                        data={data2012}
+                        x={"quarter"}
+                        y={"earnings"}
+                    />
+                    <VictoryBar
+                        data={data2013}
+                        x={"quarter"}
+                        y={"earnings"}
+                    />
+                    <VictoryBar
+                        data={data2014}
+                        x={"quarter"}
+                        y={"earnings"}
+                    />
+                    <VictoryBar
+                        data={data2015}
+                        x={"quarter"}
+                        y={"earnings"}
+                    />
+                </VictoryStack>
+            </VictoryChart>
+        </div>
     );
 }
 
